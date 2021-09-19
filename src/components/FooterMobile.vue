cbb97656fae00cb18da3ffecc2ee99021577e49a
Jalu Pujo <jalurumekso@gmail.com>
2021-09-19 16:10:54 +0700

    improve mobile footer

--- src/components/FooterMobile.vue
+++ src/components/FooterMobile.vue
@@ -1,27 +1,19 @@
 <template>
-  <div class="d-block d-lg-none container">
+  <div class="d-block d-lg-none container fixed-bottom bg-white">
     <div class="d-flex justify-content-between">
-      <button class="btn text-muted">
+      <router-link
+        v-for="menu in menus"
+        :key="menu.id"
+        :to="menu.route"
+        class="btn link-secondary"
+      >
         <i
-          class="bi bi-house-door d-block"
+          class="d-block"
+          :class="menu.icon"
           style="font-size: 1.75rem; margin-bottom: -8px"
         ></i>
-        <small style="font-size: 12px">Home</small>
-      </button>
-      <button class="btn text-muted">
-        <i
-          class="bi bi-award d-block"
-          style="font-size: 1.75rem; margin-bottom: -8px"
-        ></i>
-        <small style="font-size: 12px">Promotions</small>
-      </button>
-      <button class="btn text-muted">
-        <i
-          class="bi bi-list d-block"
-          style="font-size: 1.75rem; margin-bottom: -8px"
-        ></i>
-        <small style="font-size: 12px">Others</small>
-      </button>
+        <small style="font-size: 12px">{{ menu.title }}</small>
+      </router-link>
     </div>
   </div>
 </template>
@@ -29,6 +21,31 @@
 <script>
 export default {
   name: 'FooterMobile',
+
+  data() {
+    return {
+      menus: [
+        {
+          id: 1,
+          title: 'Home',
+          icon: 'bi bi-house-door',
+          route: '/',
+        },
+        {
+          id: 2,
+          title: 'Promotions',
+          icon: 'bi bi-award',
+          route: '/promotions',
+        },
+        {
+          id: 1,
+          title: 'Home',
+          icon: 'bi bi-list',
+          route: '/faq',
+        },
+      ],
+    };
+  },
 };
 </script>
 
