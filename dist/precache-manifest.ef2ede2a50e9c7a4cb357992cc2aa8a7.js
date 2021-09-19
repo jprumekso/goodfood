acfa80cf30579e3db58e39a27062c5e492a92d19
Jalu Pujo <jalurumekso@gmail.com>
2021-09-19 11:11:47 +0700

    add simple server for heroku deployment

---
+++ dist/precache-manifest.ef2ede2a50e9c7a4cb357992cc2aa8a7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc38578a3eef3a0c30d",
+    "url": "/css/app.7b493e13.css"
+  },
+  {
+    "revision": "9a3eec9ea55f102dbd5d",
+    "url": "/css/chunk-vendors.ab18d4a2.css"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/fonts/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/fonts/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "be57871444acaa26c0778e1354b95a76",
+    "url": "/img/background.be578714.png"
+  },
+  {
+    "revision": "71364213b16099562f944901cb0bf7b4",
+    "url": "/img/cupcake-choco.71364213.png"
+  },
+  {
+    "revision": "757c8a631b97d6374672b420c42a0ddd",
+    "url": "/img/cupcake.757c8a63.png"
+  },
+  {
+    "revision": "4d9ed058a967306caea23430a85bbceb",
+    "url": "/img/doner-kebab.4d9ed058.png"
+  },
+  {
+    "revision": "d3e4857d10a72be31d4fe3d7d50be97c",
+    "url": "/img/doughnut-milk.d3e4857d.png"
+  },
+  {
+    "revision": "f4f045acba87dc7da31b4e56f283bf39",
+    "url": "/img/doughnut-unicorn.f4f045ac.png"
+  },
+  {
+    "revision": "cce11dec82b97278351501ca475a0ed6",
+    "url": "/img/doughnut.cce11dec.png"
+  },
+  {
+    "revision": "8c20796b5926523e3db9c8f045251891",
+    "url": "/img/elemes.8c20796b.png"
+  },
+  {
+    "revision": "3d218882f61363a81f68232545947121",
+    "url": "/img/hero.3d218882.png"
+  },
+  {
+    "revision": "6d3ed042df8c84790d6dfef377bcb013",
+    "url": "/img/kathi-kebab.6d3ed042.png"
+  },
+  {
+    "revision": "b7e62d92db48a02e41896baabdd41f03",
+    "url": "/img/kebab.b7e62d92.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d09d6ec3cfb011c0e118a2f7504a1fea",
+    "url": "/img/pizza-meat.d09d6ec3.png"
+  },
+  {
+    "revision": "1baec6ed4de2324f2bc419e94553df3a",
+    "url": "/img/pizza-paperoni.1baec6ed.png"
+  },
+  {
+    "revision": "51f623e65094b4db546e653d274335f2",
+    "url": "/img/pizza.51f623e6.png"
+  },
+  {
+    "revision": "5733306f7844c71791c34c3cff757783",
+    "url": "/img/salmon-roll.5733306f.png"
+  },
+  {
+    "revision": "90149f19778f9d12c1cd55e16fc40c97",
+    "url": "/img/salmon.90149f19.png"
+  },
+  {
+    "revision": "1c372cfeb0232236738c003f321a8866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71fae03ec09a2e318a60",
+    "url": "/js/about.ea833671.js"
+  },
+  {
+    "revision": "dcc38578a3eef3a0c30d",
+    "url": "/js/app.3b17c321.js"
+  },
+  {
+    "revision": "9a3eec9ea55f102dbd5d",
+    "url": "/js/chunk-vendors.c0f18a3a.js"
+  },
+  {
+    "revision": "52f631b701b18483b9e54ca11f5739a1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+const server = express();
+
+server.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+const port = process.env.PORT || 3000;
+applicationCache.listen(port);
+
+console.log(`listening on port: ${port}`);
